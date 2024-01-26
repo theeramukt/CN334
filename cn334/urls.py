208b43201abf0bfcfe41a89ddc98370f9341e6a8
theeramukt <chayanit9336@gmail.com>
2024-01-26 19:50:22 +0700

    Create page views that contain page name and routing urls

--- cn334/urls.py
+++ cn334/urls.py
@@ -22,5 +22,10 @@ from ecommerce import views as ecom_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ecommerce/', ecom_views.ecommerce_index_view),
-    path('ecommerce/item/<item_id>', ecom_views.item_view)
+    path('ecommerce/item/<item_id>', ecom_views.item_view),
+    path('ecommerce/home', ecom_views.home_view),
+    path('ecommerce/category', ecom_views.category_view),
+    path('ecommerce/product', ecom_views.product_view),
+    path('ecommerce/checkout', ecom_views.checkout_view),
+    path('ecommerce/contact', ecom_views.contact_view),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -14,3 +14,18 @@ def item_view(request, item_id):
     }
 
     return render(request, 'index.html', context = context_data)
+
+def home_view(request):
+    return HttpResponse('Home Page')
+
+def category_view(request):
+    return HttpResponse('Category Page')
+
+def product_view(request):
+    return HttpResponse('Product Page')
+
+def checkout_view(request):
+    return HttpResponse('Checkout Page')
+
+def contact_view(request):
+    return HttpResponse('Contact Page')
